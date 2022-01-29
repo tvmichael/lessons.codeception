8c0486843b3366ccc14c7225c1367b7c077c0806
Michael Torchuk <michael.tmv@outlook.com>
2022-01-29 12:55:16 +0200

    lesson-2

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -7,28 +7,53 @@ class FirstCest
     }
 
     // tests
-    public function tryToTest(AcceptanceTester $I)
+//    public function tryToTest(AcceptanceTester $I)
+//    {
+//        $I->amOnPage('/');
+//        $I->waitForElement('#w0', 3);
+//        $I->seeElement('.jumbotron');
+//        $I->wait(1);
+//        $I->see('About', ['css' => 'body a']);
+//        $I->click("a[href='/site/about']");
+//        $I->wait(3);
+//        $I->see('This is the About page.');
+//        $I->wait(1);
+//        $I->click(['link' => 'Home']);
+//        $I->wait(1);
+//        $I->see('Congratulations');
+//        $I->wait(1);
+//        $I->click(['link' => 'Contact']);
+//        $I->see('If you have business inquiries or other questions');
+//
+//        $I->appendField('#contactform-name', 'SelectValue');
+//        $I->wait(1);
+//        $I->appendField('#contactform-email', 'select.value@mail.ua');
+//        $I->wait(1);
+//        $I->appendField('#contactform-subject', 'Select-Value');
+//    }
+
+    public function tryToSite(AcceptanceTester $I)
     {
         $I->amOnPage('/');
-        $I->waitForElement('#w0', 3);
-        $I->seeElement('.jumbotron');
+        $I->waitForElement('#allrecords', 3);
+        $I->seeElement('.wrapper-container');
         $I->wait(1);
-        $I->see('About', ['css' => 'body a']);
-        $I->click("a[href='/site/about']");
-        $I->wait(3);
-        $I->see('This is the About page.');
-        $I->wait(1);
-        $I->click(['link' => 'Home']);
-        $I->wait(1);
-        $I->see('Congratulations');
-        $I->wait(1);
-        $I->click(['link' => 'Contact']);
-        $I->see('If you have business inquiries or other questions');
-
-        $I->appendField('#contactform-name', 'SelectValue');
-        $I->wait(1);
-        $I->appendField('#contactform-email', 'select.value@mail.ua');
-        $I->wait(1);
-        $I->appendField('#contactform-subject', 'Select-Value');
+        $I->see('Отправить заявку', ['css' => 'a.order-button-top']);
+//        $I->click("a[href='/site/about']");
+//        $I->wait(3);
+//        $I->see('This is the About page.');
+//        $I->wait(1);
+//        $I->click(['link' => 'Home']);
+//        $I->wait(1);
+//        $I->see('Congratulations');
+//        $I->wait(1);
+//        $I->click(['link' => 'Contact']);
+//        $I->see('If you have business inquiries or other questions');
+//
+        $I->appendField(".t-input-block>input[name=name]", 'SelectName');
+        $I->wait(5);
+        $I->appendField(".t-input-block>input[name=email]", 'select.value@mail.ua');
+        $I->wait(5);
+//        $I->appendField('#contactform-subject', 'Select-Value');
     }
 }
